b57dcc972bdd37efc4eb7ec8dda34798059e65a3
Kevin ANANDOUT <anandout.kevin@gmail.com>
2014-11-02 18:37:17 +0100

    Formulaire de connexion fonctionnel

--- Developpement/Code/path/src/VTCalendar/CalendarBundle/Controller/IndexController.php
+++ Developpement/Code/path/src/VTCalendar/CalendarBundle/Controller/IndexController.php
@@ -3,11 +3,23 @@
 namespace VTCalendar\CalendarBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class IndexController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CalendarBundle:Default:index.html.twig');
+        $session = new Session();
+        
+        if($session->get('user') != null){
+            return $this->render('CalendarBundle:Default:index.html.twig', array('user' => $session->get('user')));
+        }
+        
+        else{
+            return $this->render('CalendarBundle:Default:login.html.twig');
+        }
+        
     }
 }
--- Developpement/Code/path/src/VTCalendar/CalendarBundle/Controller/LoginController.php
+++ Developpement/Code/path/src/VTCalendar/CalendarBundle/Controller/LoginController.php
@@ -3,11 +3,57 @@
 namespace VTCalendar\CalendarBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class LoginController extends Controller
 {
-    public function loginAction()
+    public function loginAction(Request $request)
     {
+        if($request->getMethod() == 'POST'){
+            
+            $conn = $this->get('database_connection');
+            $userMail=$request->get('mail');
+            $userPassword=$request->get('mdp');
+            
+            $user = $conn->fetchAll('SELECT login, codeProf FROM login_prof WHERE login = ? AND motPasse = ?', array($userMail, md5($userPassword)));
+        
+            if($user){
+                
+                $session = new Session();
+                $session->start();
+                $session->set('user', $user);
+                
+                return new RedirectResponse($this->generateUrl('calendar_home_page'));
+            }   
+            
+            else{
+        
+                 return $this->render('CalendarBundle:Default:login.html.twig');
+            }
+        }
+        
         return $this->render('CalendarBundle:Default:login.html.twig');
     }
+    
+    
+    public function logoutAction()
+    {
+        $session = new Session();
+        
+        $_SESSION = array();
+        
+        if (ini_get("session.use_cookies")) {
+    
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+            $session->invalidate();
+    
+        }
+        
+        //return $this->render('IbiscReservationBundle:Default:login.html.twig', array('alert' => 0));
+        return new RedirectResponse($this->generateUrl('calendar_login_page'));
+    }
+
 }
