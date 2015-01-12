9a0b9d3ca6f7ce15d2998409da60e33e374de46b
Christelle AYANGMA <christelleayangma@yahoo.fr>
2015-01-12 10:40:29 +0100

    login : gestion perte mote de passe/creation compte
    - finalisation de la création d’un nouveau compte
    - finalisation de la gestion de la perte de mot de passe - reste le
    changement effectif du mot de passe dans la base de données

--- Developpement/Code/path/src/VTCalendar/CalendarBundle/Controller/LoginController.php
+++ Developpement/Code/path/src/VTCalendar/CalendarBundle/Controller/LoginController.php
@@ -59,53 +59,115 @@ class LoginController extends Controller
     }
     
     // Fonction d'envoi de mail suite à l'oubli de son mot de passe
-    public function sendMailAction(){
+    public function sendMailAction(Request $request){
+        
+        $session = new Session();
+        $user = $session->get('user_id');
+        
+        //partie récupération du mail de l'user
+        $conn = $this->get('database_connection');
+        
+        $User = $request->get('user');
+        
+        $sql = $conn->executeQuery('SELECT codeProf, emailProf FROM login_prof WHERE login = ?', array ($User));
+        
+        $mail = $sql->fetch();
+        //$requete = mysql_query($sql, $conn) or die( mysql_error() );
+                  
+        
+        $twig = $this->get('twig'); // Twig_Environment
+        //$template = $twig->loadTemplate('CalendarBundle:Default:mailTemplate.html.twig');
+        //$parameters  = array('user' => $user);
+        //$subject  = $template->renderBlock('content', $parameters);
+        
         $message = \Swift_Message::newInstance()
         ->setSubject('Visual Time Tabling - Oubli de votre mot de passe')
         ->setFrom('do-not-reply@ibiscreservation.fr')
-        ->setTo('christelleayangma@yahoo.fr')
-        ->setBody('<h1> test </h1>', 'text/html');
+        ->setTo($mail['emailProf'])
+        ->setBody($this->render( 'CalendarBundle:Default:mailTemplate.html.twig' ),array($mail['codeProf']), 'text/html');
         
         $this->get('mailer')->send($message);
         
         return $this->render('CalendarBundle:Default:login.html.twig');
     }
      
-    // Fonction d'affichage de la page d'oubli de mot de passe
+    // Fonction d'affichage de la page d'oubli de mot de passe - où on rentre le mail pour l'envoi de mail
     public function affichageOubliMdpAction(){
         return $this->render('CalendarBundle:Default:oubliMdp.html.twig');
     }
-        
-    // Fonction de vérification pour le changement de mot de passe - à terminer lors de l'ajout de mail
-    public function testOubliMdpAction(Request $request)
-    {
-        $connection = mysql_connect( "localhost", "root", "root" ) ;
-        $db  = mysql_select_db( "VT" ) ;
-        
-        $nomUser = $request->get('user');
-        
-        return $this->render('CalendarBundle:Default:oubliMdp.html.twig');
-        
+    
+     
+    // Fonction d'affichage de la page de changement de mot de passe
+    public function affichageChangementMdpAction(){
+        $error = 0;
+        return $this->render('CalendarBundle:Default:changementMdp.html.twig', array('error' => $error));
     }
+    
+    
+    // Fonction de changement de mot de passe
+    public function verificationNouveauMdpAction (Request $request){
+      
+      // connection à la BD
+      $conn = $this->get('database_connection');
+      
+      //récupération du nouveau mot de passe entré
+      $newMdp = $request->get('newPass');
+      $cNewMdp = $request->get('confirmPass');
+      
+      //vérification si les 2 corréespondent
+      if ($newMdp != $cNewMdp) {
+        $error = 1;
+	return $this->render('CalendarBundle:Default:changementMdp.html.twig', array('error' => $error));
+      }
+      
+      else{
+           $conn->executeQuery('UPDATE login_prof SET motPasse = ?', array(md5($newMdp)));
+           return $this->render('CalendarBundle:Default:login.html.twig');
+      }
+      
+    }  
+    
      
     // Fonction d'affichage de la page de création d'un compte utilisateur
     public function affichageCreationCompteAction() {
         return $this->render('CalendarBundle:Default:creationCompte.html.twig');
     }
+    
      
      //Fonction de création d'un compte utilisateur - à terminer lors de l'ajout de mail
       public function creationCompteAction(Request $request){
+         
+        //connection à la base - à voir pour utiliser fichier ou les paramètres sont déja déclarés
         $connection = mysql_connect( "localhost", "root", "root" ) ;
         $db  = mysql_select_db( "VT" ) ;
         
         $nomUser = $request->get('nom');
+        $prenomUser = $request->get('prenom');
+        $mailUser = $request->get('mail');
+        $passUser = $request->get('pass');
+        $cpassUser = $request->get('cpass');
         
-        $sql = "INSERT INTO utilisateurs(login) VALUES('$nomUser')";
+        //pour TEST, voir comment générer des clés unique à partir de la dernière clé de la base
+         
+        //vérification mot de passe 
+        if ($passUser != $cpassUser) {
+        $error = 1;
+	return $this->render('CalendarBundle:Default:creationCompte.html.twig', array('error' => $error));
+      }
+      
+      else{
+          
+           $PassUser = md5($passUser);
+           $sql = "INSERT INTO login_prof (codeProf, login, motPasse, emailProf) VALUES(' ', '$nomUser', '$PassUser', '$mailUser')";
             
-        $requete = mysql_query($sql, $connection) or die( mysql_error() );
+           $requete = mysql_query($sql, $connection) or die( mysql_error() );
         
-        echo "user ajouté !!";
-        return $this->render('CalendarBundle:Default:login.html.twig');
+           echo "user ajouté !!";
+           return $this->render('CalendarBundle:Default:login.html.twig');
+           
+      }     
+        
+   
     }
      
 
