a7076c8e265809f86120f74a5863d092f84dfe46
Kevin ANANDOUT <anandout.kevin@gmail.com>
2015-01-13 15:49:48 +0100

    Amélioration ajout et suppression d'une séance. Les différentes tables intervenants pour une séances sont prises en compte dans le traitement.

--- Developpement/Code/path/src/VTCalendar/CalendarBundle/Controller/IndexController.php
+++ Developpement/Code/path/src/VTCalendar/CalendarBundle/Controller/IndexController.php
@@ -19,10 +19,18 @@ class IndexController extends Controller
             $conn = $this->get('database_connection');
             
             //Requête pour permettre l'affichage des séances pour un prof connecté
+<<<<<<< .merge_file_mOf74Y
+            $event = $conn->fetchAll('  SELECT DISTINCT seances.codeSeance, seances.dateSeance, seances.heureSeance, seances.dureeSeance, seances.Commentaire, enseignements.nom 
+                                        FROM enseignements, seances
+                                        WHERE enseignements.codeEnseignement = seances.codeEnseignement 
+                                        AND seances.deleted = 0'
+                                    );
+=======
             $event = $conn->fetchAll('  SELECT seances.codeSeance, seances.dateSeance, seances.heureSeance, seances.dureeSeance, seances.Commentaire, enseignements.nom 
                                         FROM enseignements, seances
                                         WHERE enseignements.codeEnseignement = seances.codeEnseignement 
                                         ');
+>>>>>>> .merge_file_XXkcuK
             
             // Requêtes pour liste déroulante - formulaire Enseignement
             $sqlmatiere = $conn->fetchAll('SELECT MATIERES.codeMatiere, MATIERES.nom FROM MATIERES ORDER BY MATIERES.nom');
@@ -31,7 +39,12 @@ class IndexController extends Controller
             $sqltypesalle = $conn->fetchAll('SELECT TYPES_SALLES.codeTypeSalle, TYPES_SALLES.nom FROM TYPES_SALLES ORDER BY TYPES_SALLES.nom');
             $sqlcomposante = $conn->fetchAll('SELECT COMPOSANTES.codeComposante, COMPOSANTES.nom FROM COMPOSANTES');
             $sqltypeactivite = $conn->fetchAll('SELECT TYPES_ACTIVITES.codeTypeActivite, TYPES_ACTIVITES.nom FROM TYPES_ACTIVITES ORDER BY TYPES_ACTIVITES.nom');
+<<<<<<< .merge_file_mOf74Y
+            $sqlgroupeenseignement = $conn->fetchAll('SELECT codeGroupe, nom FROM ressources_groupes ORDER BY nom');
+            $sqlsalles = $conn->fetchAll('SELECT DISTINCT codeSalle, nom FROM ressources_salles ORDER BY nom');
+=======
             
+>>>>>>> .merge_file_XXkcuK
             $sqlemail = $conn->executeQuery('SELECT emailProf
             							FROM LOGIN_PROF
             							WHERE codeProf = ?', array($user));
@@ -45,7 +58,11 @@ class IndexController extends Controller
              
              // Requêtes pour liste déroulante - formulaire Séance
             $sqlenseignement = $conn->fetchAll('SELECT enseignements.codeEnseignement, enseignements.nom FROM enseignements');
+<<<<<<< .merge_file_mOf74Y
+            return $this->render('CalendarBundle:Default:index.html.twig', array('user' => $session->get('user'), 'events' => $event, 'lstSqlMatiere' => $sqlmatiere, 'lstSqlZoneSalle' => $sqlzonesalle, 'lstSqlNiveau' => $sqlniveau, 'lstSqlType' => $sqltypesalle, 'lstSqlEnseignement' => $sqlenseignement, 'lstSqlComposante' => $sqlcomposante, 'lstSqlTypeActivite' => $sqltypeactivite, 'msgmail' => $nomail, 'lstSqlGroupes' => $sqlgroupeenseignement, 'lstSqlSalles' => $sqlsalles));
+=======
             return $this->render('CalendarBundle:Default:index.html.twig', array('user' => $session->get('user'), 'events' => $event, 'lstSqlMatiere' => $sqlmatiere, 'lstSqlZoneSalle' => $sqlzonesalle, 'lstSqlNiveau' => $sqlniveau, 'lstSqlType' => $sqltypesalle, 'lstSqlEnseignement' => $sqlenseignement, 'lstSqlComposante' => $sqlcomposante, 'lstSqlTypeActivite' => $sqltypeactivite, 'msgmail' => $nomail));
+>>>>>>> .merge_file_XXkcuK
             
             
         }
@@ -135,8 +152,38 @@ class IndexController extends Controller
                 $dateCreation = new \DateTime();
                 $commentaire = $request->get('commentaire');
                 
+                $groupe = $request->get('groupes');
+                $salle = $request->get('salles');
+                
+                $selectRessourceProf = $conn->executeQuery('SELECT codeRessource FROM enseignements_profs WHERE codeEnseignement = ? ', array($enseignement['codeEnseignement']));
+                $ressourceProf = $selectRessourceProf->fetch();
+                
+                
                 $conn->executeQuery('INSERT INTO seances (codeSeance, dateSeance, heureSeance, dureeSeance, codeEnseignement, dateCreation, deleted, codeProprietaire, commentaire, diffusable)
                                      VALUES (0, ?, ?, ?, ?, ?, 0, 777, ?, 0)', array($dateSeance->format('Y-m-d'), $heureSeance, $enseignement['dureeSeance'], $enseignement['codeEnseignement'], $dateCreation->format('Y-m-d H:i:sP'), $commentaire));
+                
+                $selectLastIdInserted = $conn->executeQuery('SELECT LAST_INSERT_ID() as lastId FROM seances');
+                $lastIdInserted = $selectLastIdInserted->fetch();
+                
+                $selectLastSeanceInserted = $conn->executeQuery('SELECT * FROM seances WHERE codeSeance = ?', array($lastIdInserted['lastId']));
+                $selectLastSeance = $selectLastSeanceInserted->fetch();
+                
+                //seance profs
+                
+                $conn->executeQuery('INSERT INTO seances_profs (codeSeance, codeRessource, dateModif, deleted, codeProprietaire)
+                                     VALUES (?, ?, ?, 0, 777)', array($lastIdInserted['lastId'], $ressourceProf['codeRessource'], $dateCreation->format('Y-m-d H:i:sP')));
+                
+                //seance salles
+                
+                $conn->executeQuery('INSERT INTO seances_salles (codeSeance, codeRessource, dateModif, deleted, codeProprietaire)
+                                     VALUES (?, ?, ?, 0, 777)', array($lastIdInserted['lastId'], $salle, $dateCreation->format('Y-m-d H:i:sP')));
+                 
+                //seance groupes
+               
+                if($groupe != 0){
+                $conn->executeQuery('INSERT INTO seances_groupes (codeSeance, codeRessource, dateModif, deleted, codeProprietaire)
+                                     VALUES (?, ?, ?, 0, 777)', array($lastIdInserted['lastId'], $groupe, $dateCreation->format('Y-m-d H:i:sP')));
+                }
                 
                 $updateDureeTotale = $enseignement['dureeTotale'] - $enseignement['dureeSeance'];
                 $conn->executeQuery('UPDATE enseignements SET dureeTotale = ? WHERE codeEnseignement = ?' , array($updateDureeTotale, $enseignement['codeEnseignement']));
@@ -167,7 +214,15 @@ class IndexController extends Controller
             $updateDureeTotale = $dureeTotaleEnseignement['dureeTotale'] + $seance['dureeSeance'];
             
             
+<<<<<<< .merge_file_mOf74Y
+            
+            $conn->executeQuery('UPDATE seances SET deleted = 1 WHERE codeSeance = ?', array($request->get('codeSeance')));
+            $conn->executeQuery('UPDATE seances_profs SET deleted = 1 WHERE codeSeance = ?', array($request->get('codeSeance')));
+            $conn->executeQuery('UPDATE seances_salles SET deleted = 1 WHERE codeSeance = ?', array($request->get('codeSeance')));
+            $conn->executeQuery('UPDATE seances SET deleted = 1 WHERE codeSeance = ?', array($request->get('codeSeance')));
+=======
             $conn->executeQuery('DELETE FROM seances WHERE codeSeance = ?', array($request->get('codeSeance')));
+>>>>>>> .merge_file_XXkcuK
             $conn->executeQuery('UPDATE enseignements SET dureeTotale = ? WHERE codeEnseignement = ?', array($updateDureeTotale, $seance['codeEnseignement']));
             
             return new RedirectResponse($this->generateUrl('calendar_home_page'));         
