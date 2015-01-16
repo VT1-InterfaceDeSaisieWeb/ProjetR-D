6da511c19b8205f06f348c34aaeff8cb36b9e67e
Christelle AYANGMA <christelleayangma@yahoo.fr>
2015-01-16 19:32:57 +0100

    gestion du calendrier - filtre des séances
    - ajoute de la possibilité à l’utilisateur de choisir d’afficher que
    ses séances ou toutes les séances de la BD
    - bug à corriger

--- Developpement/Code/path/src/VTCalendar/CalendarBundle/Controller/IndexController.php
+++ Developpement/Code/path/src/VTCalendar/CalendarBundle/Controller/IndexController.php
@@ -59,6 +59,57 @@ class IndexController extends Controller
         
     }
     
+     public function filtreAction()
+    {
+         
+          $session = new Session();
+        
+        if($session->get('user_id') != null){
+            
+            $user = $session->get('user_id');
+            $conn = $this->get('database_connection');
+            
+            //Requête pour permettre l'affichage des séances pour un prof connecté
+            $event = $conn->fetchAll('  SELECT seances.codeSeance, seances.dateSeance, seances.heureSeance, seances.dureeSeance, seances.Commentaire, enseignements.nom 
+                                        FROM enseignements, seances
+                                        WHERE enseignements.codeEnseignement = seances.codeEnseignement 
+                                        ');
+            
+            
+            // Requêtes pour liste déroulante - formulaire Enseignement
+            $sqlmatiere = $conn->fetchAll('SELECT MATIERES.codeMatiere, MATIERES.nom FROM MATIERES ORDER BY MATIERES.nom');
+            $sqlzonesalle = $conn->fetchAll('SELECT ZONES_SALLES.codeZoneSalle, ZONES_SALLES.nom FROM ZONES_SALLES ORDER BY ZONES_SALLES.nom');
+            $sqlniveau = $conn->fetchAll('SELECT NIVEAUX.codeNiveau, NIVEAUX.nom FROM NIVEAUX ORDER BY NIVEAUX.nom');
+            $sqltypesalle = $conn->fetchAll('SELECT TYPES_SALLES.codeTypeSalle, TYPES_SALLES.nom FROM TYPES_SALLES ORDER BY TYPES_SALLES.nom');
+            $sqlcomposante = $conn->fetchAll('SELECT COMPOSANTES.codeComposante, COMPOSANTES.nom FROM COMPOSANTES');
+            $sqltypeactivite = $conn->fetchAll('SELECT TYPES_ACTIVITES.codeTypeActivite, TYPES_ACTIVITES.nom FROM TYPES_ACTIVITES ORDER BY TYPES_ACTIVITES.nom');
+            
+            $sqlemail = $conn->executeQuery('SELECT emailProf
+            							FROM LOGIN_PROF
+            							WHERE codeProf = ?', array($user));
+            $hasMail = $sqlemail->fetch();
+            if(empty($hasMail['emailProf'])){
+            	$nomail = 1;
+            }
+            else{
+            	$nomail = 0;
+            }
+             
+             // Requêtes pour liste déroulante - formulaire Séance
+            $sqlenseignement = $conn->fetchAll('SELECT enseignements.codeEnseignement, enseignements.nom FROM enseignements');
+            return $this->render('CalendarBundle:Default:index.html.twig', array('user' => $session->get('user'), 'events' => $event, 'lstSqlMatiere' => $sqlmatiere, 'lstSqlZoneSalle' => $sqlzonesalle, 'lstSqlNiveau' => $sqlniveau, 'lstSqlType' => $sqltypesalle, 'lstSqlEnseignement' => $sqlenseignement, 'lstSqlComposante' => $sqlcomposante, 'lstSqlTypeActivite' => $sqltypeactivite, 'msgmail' => $nomail));
+            
+            
+        }
+        
+        else{
+            return $this->render('CalendarBundle:Default:login.html.twig');
+        }
+         
+         
+    }
+    
+    
     
     public function ajoutMatiereAction(Request $request)
     {
